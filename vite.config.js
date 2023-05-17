ed0942bd659e7cc62b3a33a33eee2096a6e8126f
vickyrajput5 <waqarmuhammad337@gmail.com>
2023-05-18 03:43:39 +0500

    'c'

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/movie/index.html',
+  base:'http://127.0.0.1:5173/',
   plugins: [react()],
 })

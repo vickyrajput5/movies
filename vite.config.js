d040646ea305102edfc63a88453b85d582d8ee45
vickyrajput5 <waqarmuhammad337@gmail.com>
2023-05-19 00:24:50 +0500

    add: deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'http://127.0.0.1:5173/',
+  
   plugins: [react()],
+  base:'/movies/',
 })
